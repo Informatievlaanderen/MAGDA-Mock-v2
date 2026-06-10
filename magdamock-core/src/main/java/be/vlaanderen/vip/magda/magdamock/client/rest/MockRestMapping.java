7280de6abd5506ed87f17d47d9884ab59f1fc40c
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2026-06-10 15:16:34 +0000

    Merged in feat/MCAA-2973 (pull request #71)
    feat: add socialeHuisvesting dossierZoeken POST mapping
    
    * feat: add socialeHuisvesting dossierZoeken POST mapping
    
    
    Approved-by: Niels Desmet

--- magdamock-core/src/main/java/be/vlaanderen/vip/magda/magdamock/client/rest/MockRestMapping.java
+++ magdamock-core/src/main/java/be/vlaanderen/vip/magda/magdamock/client/rest/MockRestMapping.java
@@ -2,10 +2,14 @@ package be.vlaanderen.vip.magda.magdamock.client.rest;
 
 import java.util.List;
 
-public record MockRestMapping(List<String> folderPath, List<String> queryParameters, Integer urlParametersSize, String url, String method) {
+public record MockRestMapping(List<String> folderPath, List<String> queryParameters, Integer urlParametersSize, List<String> requestBodyParameters, String url, String method) {
     // url parameters should be marked with %s, as such they can be filled in with String.format
     public static final List<MockRestMapping> MAPPINGS = List.of(
-            new MockRestMapping(List.of("v1", "mobility", "registrations", "get", "vin"), List.of("vin"), 0, "/v1/mobility/registrations", "GET"),
-            new MockRestMapping(List.of("v1", "mobility", "registrations", "get", "plateNr"), List.of("plateNr"), 0, "/v1/mobility/registrations", "GET")
+            // Mobility
+            new MockRestMapping(List.of("v1", "mobility", "registrations", "get", "vin"), List.of("vin"), 0, List.of(), "/v1/mobility/registrations", "GET"),
+            new MockRestMapping(List.of("v1", "mobility", "registrations", "get", "plateNr"), List.of("plateNr"), 0, List.of(), "/v1/mobility/registrations", "GET"),
+
+            // SocZek
+            new MockRestMapping(List.of("v1", "socZek", "socialeHuisvesting", "dossiersZoeken", "post", "dossierStatus"), List.of(), 0, List.of("dossierStatus"), "/v1/socZek/socialeHuisvesting/dossiers/zoeken", "POST")
     );
 }
