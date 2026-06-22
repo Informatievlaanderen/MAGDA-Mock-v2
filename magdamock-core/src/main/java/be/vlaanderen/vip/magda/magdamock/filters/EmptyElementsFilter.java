3e2b38c9049e61cef254a68c53ae3b97a5cb65c8
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2026-06-22 08:08:05 +0000

    Merged in feat/MCAA-3341-xml-empty-elements-filter (pull request #81)
    feat(MCAA-3341): add filter to remove empty elements from xml response
    
    * feat(MCAA-3341): add filter to remove empty elements from xml response
    
    
    Approved-by: Bart Peeters

---
+++ magdamock-core/src/main/java/be/vlaanderen/vip/magda/magdamock/filters/EmptyElementsFilter.java
@@ -0,0 +1,47 @@
+package be.vlaanderen.vip.magda.magdamock.filters;
+
+import be.vlaanderen.vip.magda.client.MagdaDocument;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+
+public class EmptyElementsFilter implements MagdaMockFilter {
+
+    private static final String XSLT = """
+        <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+          <xsl:template match="*[not(@*) and not(*) and not(normalize-space())]"/>
+          
+          <xsl:template match="node()|@*">
+            <xsl:copy>
+              <xsl:apply-templates select="node()|@*"/>
+            </xsl:copy>
+          </xsl:template>
+        </xsl:stylesheet>
+        """;
+
+    public Document filter(MagdaDocument request, Document response) {
+        if (response == null) {
+            return null;
+        }
+        try {
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(new StringReader(XSLT)));
+
+            DOMResult outputTarget = new DOMResult();
+
+            transformer.transform(new DOMSource(response), outputTarget);
+            return (Document) outputTarget.getNode();
+        } catch (TransformerException e) {
+            return response;
+        }
+    }
+}
