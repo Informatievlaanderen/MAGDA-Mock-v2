8d216cd53be2356960eb70646fe8384e6c4f4a27
bart.peeters2@vlaanderen.be <bart.peeters2@vlaanderen.be>
2026-06-22 13:03:08 +0200

    fix: add logging, test fail on github but runs locally

--- magdamock-core/src/test/java/be/vlaanderen/vip/magda/magdamock/client/rest/RestServicesTest.java
+++ magdamock-core/src/test/java/be/vlaanderen/vip/magda/magdamock/client/rest/RestServicesTest.java
@@ -480,6 +480,7 @@ class RestServicesTest {
             String expectedMappingType
     ) {
         log.info("Request :" + mockRestRequest);
+        System.out.println("Request :" + mockRestRequest);
         var response = magdaMockConnection.sendRestRequest(mockRestRequest);
         Assertions.assertNotNull(response);
         Assertions.assertEquals(expectedMessage, response.body().get("message").textValue());
